3b504bf90e33af0275140e508e1ae65d3a19f3bd
WangNing-Zhou <1879154660@qq.com>
2023-03-18 14:16:28 +0800

    添加网页结构

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-window.onload(() => {
+function  calculationFont(){
     // 获取屏幕宽度
     let screenWidth = document.documentElement.clientWidth;
-
+    //如果屏幕不为移动端,则设置浏览器的默认样式
     let fontSize = 16;
     if (screenWidth < 850) {
         // 计算根元素字体大小，这里假设设计图宽度为750px，根元素字体大小为10px
         fontSize = screenWidth / 42;
     }
-
 // 设置给html标签
     document.documentElement.style.fontSize = fontSize + 'px';
+}
 
 
-})()
+
